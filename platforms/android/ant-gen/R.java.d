358d6b13c4addd184e43484acae09969c183828a
hopalicious <hopalicious@158.38.184.197>
2014-03-25 12:21:56 +0100

    Merge origin/master
    Conflicts:
    	README.md

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -6,4 +6,9 @@ C:\Users\hopalicious\Documents\NetBeansProjects\seed\seed\platforms\android\res\
 C:\Users\hopalicious\Documents\NetBeansProjects\seed\seed\platforms\android\res\drawable-xhdpi\icon.png \
 C:\Users\hopalicious\Documents\NetBeansProjects\seed\seed\platforms\android\res\values\strings.xml \
 C:\Users\hopalicious\Documents\NetBeansProjects\seed\seed\platforms\android\res\xml\config.xml \
+C:\Users\hopalicious\Documents\NetBeansProjects\seed\seed\platforms\android\ant-build\res\drawable\icon.png \
+C:\Users\hopalicious\Documents\NetBeansProjects\seed\seed\platforms\android\ant-build\res\drawable-hdpi\icon.png \
+C:\Users\hopalicious\Documents\NetBeansProjects\seed\seed\platforms\android\ant-build\res\drawable-ldpi\icon.png \
+C:\Users\hopalicious\Documents\NetBeansProjects\seed\seed\platforms\android\ant-build\res\drawable-mdpi\icon.png \
+C:\Users\hopalicious\Documents\NetBeansProjects\seed\seed\platforms\android\ant-build\res\drawable-xhdpi\icon.png \
 C:\Users\hopalicious\Documents\NetBeansProjects\seed\seed\platforms\android\ant-build\AndroidManifest.xml \
