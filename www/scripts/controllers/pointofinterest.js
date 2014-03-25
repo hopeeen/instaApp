056ff10b318c84910d696756f5b42a7bc3cdbac0
axel <axel@axelAcer>
2014-03-25 16:15:33 +0100

    Point of interest

--- www/scripts/controllers/pointofinterest.js
+++ www/scripts/controllers/pointofinterest.js
@@ -1,5 +1,34 @@
 App.controller('poicontroller', function($scope, dummyData) {
             
-                    $scope.interestArray = dummyData.getInterest();
+                    $scope.interests = dummyData.getInterest();
+                    
+                    
+                    var interests2 = [{
+                            interestName: 'Blue Moske',
+                            interestLocation: 'Istanbul',
+                            interestRating: '5.0'
+
+                        },
+                        {
+                            interestName: 'Red Moske',
+                            interestLocation: 'Istanbul',
+                            interestRating: '6.0'
+
+                        },
+                        {
+                            interestName: 'Pink Moske',
+                            interestLocation: 'Istanbul',
+                            interestRating: '3.0'
+
+                        },
+                        {
+                            interestName: 'Brown Moske',
+                            interestLocation: 'Istanbul',
+                            interestRating: '1.0'
+
+                        }];
+                    
+                    
+                    
 
 });
