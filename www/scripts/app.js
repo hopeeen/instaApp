b8b7648fd524f361e8f97a20917364369535c392
hopalicious <hopalicious@158.38.184.197>
2014-03-25 15:01:46 +0100

    Merge origin/master
    Conflicts:
    	www/index.html
    	www/scripts/app.js

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -12,7 +12,7 @@ angular.module('myApp', [
 
         config(['$routeProvider', function($routeProvider) {
                 $routeProvider.when('/map', {templateUrl: 'partials/map.html', controller: 'mapController'});
-                $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'loginController'});
+                $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginController'});
                 $routeProvider.when('/create-account', {templateUrl: 'partials/create-account.html', controller: 'create-accountController'});
                 $routeProvider.when('/achievements', {templateUrl: 'partials/achievements.html', controller: 'achievementsController'});
                 $routeProvider.when('/left-menu', {templateUrl: 'partials/left-menu.html', controller: 'left-menuController'});
