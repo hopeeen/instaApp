9e0822cfa5e42630575718d022006c078f00d223
hopalicious <hopalicious@158.38.184.197>
2014-03-25 15:16:54 +0100

    Merge origin/master
    Conflicts:
    	www/index.html
    	www/scripts/app.js

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+var app = angular.module('myApp', [
     'ngRoute',
     'myApp.filters',
     'myApp.services',
