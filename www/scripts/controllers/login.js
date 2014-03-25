4b4d3af609cc7b91c661fbd9177b15945d588346
PetterSa <petter.saelen@gmail.com>
2014-03-25 13:54:44 +0100

    teste stuff, faa meg ut av feil branch :(

---
+++ www/scripts/controllers/login.js
@@ -0,0 +1,14 @@
+'use strict';
+
+app.controller('LoginController', function($scope, daoUser) {
+  $scope.status = "Nothing to report";
+
+  $scope.login = function() {
+    daoUser.login($scope.username, $scope.password, function() {
+      $scope.status = "Successful login"
+    }, function() {
+      $scope.status = "Login error"
+      $scope.password="";
+    });
+  };
+});
