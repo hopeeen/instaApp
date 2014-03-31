50ed646ff7db5dc69e3fb4a6c700b5cfc9e384bd
hopalicious <hopalicious@158.38.185.83>
2014-03-31 16:09:28 +0200

    Merge origin/master
    Conflicts:
    	www/index.html
    	www/scripts/app.js

--- www/scripts/controllers/leftmenu.js
+++ www/scripts/controllers/leftmenu.js
@@ -4,10 +4,10 @@ app.controller('leftmenucontroller', function($scope, dummyData) {
         $scope.sjekk="haha";
         
         $scope.ShowSignIn = false;
-                $Scope.DisplaySignIn = function() {
+                $scope.DisplaySignIn = function() {
                 $scope.ShowSignIn = true;
                         };
-                $Scope.HideModal = function() {
+                $scope.HideModal = function() {
 
                 $scope.ShowSignIn = false;
                         };
