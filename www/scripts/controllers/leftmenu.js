d207da5a0310707f05e8e4bfbd7ba8e25d34fac1
Martin Hagerup <martin.hagerup@gmail.com>
2014-04-30 21:07:11 +0300

    side menu current user

--- www/scripts/controllers/leftmenu.js
+++ www/scripts/controllers/leftmenu.js
@@ -1,9 +1,9 @@
 app.controller('sideMenuController', function($scope, $rootScope, daoUser) {
     /*$rootScope.userLoggedIn = true;*/
-    if(localStorage.firstname && $rootScope.userLoggedIn) {
-        $scope.displayName = localStorage.firstname + localStorage.lastname;
-    } else {
+    if(localStorage.username) {
         $scope.displayName = localStorage.username;
+    } if(localStorage.firstname && localStorage.lastname) {
+        $scope.displayNameFirstLast = localStorage.firstname + " " + localStorage.lastname;
     };
     
     $scope.signOut = function() {
