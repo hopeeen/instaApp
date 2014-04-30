d8bd786240e1e92dd86c470a4baf2e66cd51dac4
Petter Lundemo <petter_lundemo@hotmail.com>
2014-04-30 21:36:20 +0200

    few

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -20,6 +20,7 @@ var app = angular.module('myApp', [
                 $routeProvider.when('/achievements', {templateUrl: 'partials/achievement.html', controller: 'achievementsctrl'});
                 $routeProvider.when('/left-menu', {templateUrl: 'partials/leftmenu.html', controller: 'leftmenucontroller'});
                 $routeProvider.when('/pointofinterest', {templateUrl: 'partials/pointofinterest.html', controller: 'poicontroller'});
+                $routeProvider.when('/share', {templateUrl: 'partials/share.html', controller: 'DemoCTRL'});
                 
                 $routeProvider.when('/', {redirectTo: '/mapTest'});
                 $routeProvider.otherwise({redirectTo: '/404'});
