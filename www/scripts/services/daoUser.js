9003f76f2d334c8a93287c5afa31de19541ae4a0
Petter Lundemo <petter_lundemo@hotmail.com>
2014-04-30 14:40:28 +0200

    Manage account funker nå!

--- www/scripts/services/daoUser.js
+++ www/scripts/services/daoUser.js
@@ -1,4 +1,4 @@
-app.service('daoUser', function($location, $http, REST) {
+app.service('daoUser', function($location, $http, REST, Restangular) {
   //loggedIn is used in a view and must therefore be either an object
   //or an array to be automatically updated
   var loggedIn = {};
@@ -20,9 +20,35 @@ app.service('daoUser', function($location, $http, REST) {
 
   this.login = function(username, password, successCallback, errorCallback) {
     var words = CryptoJS.enc.Utf8.parse(username + ":" + password);
+    
     var base64 = CryptoJS.enc.Base64.stringify(words);
     localStorage.userAuth = 'Basic ' + base64;
     localStorage.username = username;
+
+      
+      
+$http.get(REST.path + 'users?where=user=="' + localStorage.username + '"').success(function(result) {
+
+      var mid = JSON.stringify(result._items[0]._id).replace('"', '');
+      var mid2 = mid.replace('"', '');
+      
+localStorage.id = mid2.trim();
+var mid = JSON.stringify(result._items[0].firstname).replace('"', '');
+var mid2 = mid.replace('"', '');
+localStorage.firstname = mid2.trim();
+var mid = JSON.stringify(result._items[0].lastname).replace('"', '');
+var mid2 = mid.replace('"', '');
+localStorage.firstname = mid2.trim();
+var mid = JSON.stringify(result._items[0].description).replace('"', '');
+var mid2 = mid.replace('"', '');
+localStorage.firstname = mid2.trim();
+     
+    }).error(function(result) {
+
+    });
+      
+      
+      
     $http.defaults.headers.common.Authorization = localStorage.userAuth;
 
     $http.get(REST.path + 'auth').success(function(result) {
@@ -47,4 +73,84 @@ app.service('daoUser', function($location, $http, REST) {
     loggedIn.value = false;
     $location.path("/login");
   };
+  
+  
+  
+
+
+  
+this.update = function(name, email, description, newpassword) {
+
+
+
+
+var newuser = "";
+var sync = 0;
+ 
+
+var newuser = {user: email, description: description};
+
+var all = name.split(" ");
+var lastname = all[all.length-1];
+var firstname = "";
+for(a = 0; a < all.length-1; a++){
+    firstname = firstname + " " + all[a];
+}
+alert(lastname);
+alert(firstname);
+var newuser = {user: email, description: description, firstname: firstname, lastname: lastname};
+    $http.put(REST.path + 'users/' + localStorage.id, JSON.stringify(newuser)).success(function(result2) {
+        
+      
+      if (typeof (successCallback) == 'function') {
+        
+            successCallback();
+        
+      }
+      
+     
+    }).error(function(result2) {
+      if (typeof (errorCallback) == 'function') {
+        
+        errorCallback();
+      }
+    });
+
+
+
+
+     
+    
+
+alert(localStorage.id + "er min id");
+
+    
+    /*
+    alert("Kom hit - " + localStorage.id + " - " + localStorage.username);
+$http.get(REST.path + 'users?where=user=="' + localStorage.username + '"').success(function(result) {
+      
+      alert(JSON.stringify(result));
+      
+
+     
+    }).error(function(result) {
+
+    });
+    
+    */
+    
+    
+    
+    
+    
+    
+    
+    
+}
+  
+  
+  
+  
+  
+  
 });
